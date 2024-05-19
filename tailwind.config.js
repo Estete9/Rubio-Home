9b5d08823e4d83c74c7eb4a96c7e5ac88a571b75
Esteban <cepalacios@outlook.com>
2024-05-19 11:19:51 -0500

    add colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/*.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/*.{html,js,jsx}', './src/**/*.{html, js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#d9ad26',
+        secondary: '#d1d3d4',
+        accent: '#ec1c24',
+        neutral: '#231f20',
+      },
+    },
   },
   plugins: [require('tailwindcss-debug-screens')],
 };
+
+
+
+
 
 
 
