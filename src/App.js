7c14faade4544b7c9bcc2e909c2863e1454e4b58
Esteban <cepalacios@outlook.com>
2024-05-19 13:56:44 -0500

    Create Navbar component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   document.body.classList.add('debug-screens');
 
   return (
     <div>
-      <h1 className="font-helveticaNeue font-normal"> this text is regular</h1>
-      <h1 className="font-helveticaNeue font-light"> this text is light</h1>
-      <h1 className="font-helveticaNeue font-light italic"> this text is light italic</h1>
-      <h3 className="font-helveticaNeue font-bold italic"> this text is bold italic</h3>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+const Navbar = () => (
+  <div>
+    <div>this is the topbar</div>
+    <div>this is the second part of topbar</div>
+  </div>
+);
+
+export default Navbar;
