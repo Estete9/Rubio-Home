ff83062edd2bcd1f679a7c162c3d78e3bef10111
Esteban <cepalacios@outlook.com>
2024-05-21 11:50:35 -0500

    Update margins

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ import menu from '../assets/icons/menu.svg';
 
 const Header = () => (
   <>
-    <img className="header-iso my-3" src={iso} alt="logo iso" />
+    <img className="header-iso my-3 ml-2" src={iso} alt="logo iso" />
     <img className="header-text my-2" src={logoText} alt="logo text" />
-    <img className="menu-btn my-3" src={menu} alt="menu button" />
+    <img className="menu-btn my-3 mr-2" src={menu} alt="menu button" />
   </>
 );
 
