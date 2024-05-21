7e3fab9d4e2ceaaad3b25433965e9d8adfc9def9
Esteban <cepalacios@outlook.com>
2024-05-21 11:53:10 -0500

    Add footer to App.js and Add padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,10 @@ const Footer = () => (
   <>
     <div className="footer-top h-12 flex justify-between">
       <img className="header-iso my-3 ml-2" src={iso} alt="logo iso" />
-      <div className="footer-actions flex h-full items-center gap-1">
-        <p className="footer-actions-word text-xs font-helveticaNeue font-light text-secondary">Inicio</p>
+      <div className="footer-actions flex h-full items-center gap-1 mr-2">
+        <p className="footer-actions-word text-xs font-helveticaNeue font-light text-secondary">
+          Inicio
+        </p>
         <img className="footer-actions-chevron h-3" src={chevronUp} alt="scroll to top" />
       </div>
     </div>
