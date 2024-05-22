a26377cdecf8dafb656c03140f0200ce1b90f371
Esteban <cepalacios@outlook.com>
2024-05-22 11:26:38 -0500

    Set headline font size

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,10 +2,10 @@ import bgHero from '../assets/mobile/hero-img-mobile.png';
 import heroLogo from '../assets/logos/Jorge Rubio Logo final-white.svg';
 
 const Hero = () => (
-  <section className="hero-section relative overflow-y-scroll h-screen w-screen">
-    <img src={bgHero} alt="Kia Matriz" className="w-screen top-0 left-0 z-0" />
-    <div className="hero-content absolute z-10 flex flex-col items-center w-full mt-28 gap-12">
-      <h1 className="text-secondary font-helveticaNeue font-light text-4xl">
+  <section className="hero-section w-screen relative h-[80vh]">
+    <img src={bgHero} alt="Kia Matriz" className="w-screen z-0 absolute" />
+    <div className="hero-content absolute z-10 flex flex-col justify-center items-center w-full mt-28 gap-12">
+      <h1 className="text-secondary font-helveticaNeue font-light text-3xl mx-auto">
         ...PIONEROS EN EL
         <br />
         ARTE DE ROTULACIÓN
