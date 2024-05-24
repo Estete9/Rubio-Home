174a4fa431433dc088332c1950f7ea46286a4c5e
Esteban <cepalacios@outlook.com>
2024-05-24 13:45:11 -0500

    Implement Divider component

---
+++ src/components/Divider.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+
+const Divider = (props) => {
+  const { quote } = props;
+
+  return (
+    <section className="divider-section h-12 bg-neutral/85 flex justify-center items-center">
+      <p className="divider-text text-sm font-light italic text-secondary text-center">
+        {quote}
+      </p>
+    </section>
+  );
+};
+
+Divider.propTypes = {
+  quote: PropTypes.string.isRequired,
+};
+
+export default Divider;
