0191469286e331fe36f498a87b88b4915c6fbae5
Esteban <cepalacios@outlook.com>
2024-06-04 07:59:14 -0500

    Add top margin to section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import bgHero from '../assets/mobile/hero-img-mobile.png';
 import heroLogo from '../assets/logos/Jorge Rubio Logo final-white.svg';
 
 const Hero = () => (
-  <section className="hero-section relative">
+  <section className="hero-section relative mt-12">
     <img src={bgHero} alt="Kia Matriz" className="hero-bg section-bg" />
     <div className="hero-content flex flex-col justify-center items-center gap-12 top-[15%] w-screen absolute">
       <h1 className="hero-headline text-secondary font-helveticaNeue font-light text-3xl mx-auto">
