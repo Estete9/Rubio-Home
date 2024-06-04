1c82509332cb4b9080db7c6c98948e170f6c6fcd
Esteban <cepalacios@outlook.com>
2024-06-04 08:00:07 -0500

    Implement menu button change when clicked

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,8 @@ import PropTypes from 'prop-types';
 import { useState } from 'react';
 import iso from '../assets/logos/iso-logo.svg';
 import logoText from '../assets/logos/text-logo.svg';
-// import menu from '../assets/icons/menu.svg';
+import menu from '../assets/icons/menu.svg';
+import close from '../assets/icons/close.svg';
 import MobileMenu from './MobileMenu';
 
 const Header = ({ scrollTop }) => {
@@ -34,10 +35,12 @@ const Header = ({ scrollTop }) => {
       {isMobile && (
         <button
           type="button"
-          className="menu-btn my-3 mr-2 w-6"
+          className="menu-btn my-3 w-6 z-20"
           alt="menu button"
           onClick={toggleMenu}
-        />
+        >
+          <img src={isOpen ? close : menu} alt="menu button" />
+        </button>
       )}
       {isMobile && <MobileMenu isMobile={isMobile} isOpen={isOpen} closeMenu={closeMenu} />}
     </>
