f8516620ebaa0a0aa1f0f46579861b39ceac86b6
tivyh112 <tivyh112@gmail.com>
2024-07-29 20:43:54 +0000

    Increade divider height and update content

--- src/components/Divider.jsx
+++ src/components/Divider.jsx
@@ -4,8 +4,8 @@ const Divider = (props) => {
   const { quote } = props;
 
   return (
-    <section className="divider-section h-12 bg-neutral/80 flex justify-center items-center">
-      <p className="divider-text text-sm font-light italic text-secondary text-center">
+    <section className="divider-section h-28 bg-neutral/80 flex justify-center items-center">
+      <p className="divider-text text-sm font-light italic text-secondary text-center p-4">
         {quote}
       </p>
     </section>
